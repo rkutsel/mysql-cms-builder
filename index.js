17b14b9b3858369db7e652ae2490668dad7e7882
rkutsel <roman.kutsel@docusign.com>
2022-05-12 15:50:26 -0700

    major changes to code

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const inquirer = require("inquirer");
-const {
-	initialQuestions,
-	addDepartment,
-	addRole,
-	mainMenu,
-} = require("./dist/questions");
+const { questions } = require("./dist/questions");
 const Employee = require("./dist/employee");
 const Role = require("./dist/role");
 const Department = require("./dist/department");
+const Database = require("./dist/database");
 
 function manageDepartment(answer) {
 	if (answer === "view all departments") {
@@ -17,11 +13,11 @@ function manageDepartment(answer) {
 		newDepartment.selectDepartments(select, consoleLog);
 		setTimeout(returnToMainMenu, 250);
 	} else {
-		inquirer.prompt(addDepartment).then((answer) => {
+		inquirer.prompt(questions.addDepartment).then((answer) => {
 			const { addDepartment } = answer;
 			const newDepartment = new Department(addDepartment);
 			newDepartment.addDepartment();
-			console.log(`Department name "${addDepartment}" created!\n`);
+			console.log("\x1b[36m", `Department name "${addDepartment}" created!\n`);
 			returnToMainMenu();
 		});
 	}
@@ -35,35 +31,138 @@ function manageRole(answer) {
 		newRole.selectRoles(select, consoleLog);
 		setTimeout(returnToMainMenu, 250);
 	} else {
-		inquirer.prompt(addRole).then((answer) => {
+		inquirer.prompt(questions.addRole).then((answer) => {
 			const { roleTitle, roleSalary } = answer;
 			chooseDepartment(roleTitle, roleSalary);
-			// const newRole = new Role(roleTitle, roleSalary);
-			// newRole.addRole();
-			// console.log(`Role name "${roleTitle}" created!\n`);
-			// returnToMainMenu();
 		});
 	}
 }
 
 async function chooseDepartment(title, salary) {
-	// selectDepartments().then((data) => console.log(data));
-	const select = "SELECT * FROM department ORDER BY name";
-	const consoleLog = false;
-	const newDepartment = new Department();
-	console.log(
-		await newDepartment
-			.selectDepartments(select, consoleLog)
-			.then((data) => data)
-	);
-	// console.log(typeof selectQuery().then((data) => data));
-	// await selectQuery.forEach((element) => {
-	// 	console.log(element);
-	// });
+	const selectDepartment = `SELECT name FROM department`;
+	const departmentId = await renderedSelect(selectDepartment);
+	const question = questions.chooseDepartment;
+	const roleName = await renderedAnswer(departmentId, title, salary, question);
+}
+
+async function updateRole(title, salary, roleName) {
+	const select = `SELECT id FROM department WHERE name = ?`;
+	const arg = roleName.toUpperCase();
+	const deptId = await preparedSelect(select, arg);
+	setTimeout(() => {
+		const newRole = new Role(title, salary, deptId);
+		newRole.addRole();
+		console.log(
+			"\x1b[36m",
+			`New Entry:\n Title: ${title}\n Salary: ${salary}\n DepartmentID: ${deptId}\n has been added!`
+		);
+		returnToMainMenu();
+	}, 250);
+}
+
+function manageEmployee(answer) {
+	if (answer === "view all employees") {
+		const select = `SELECT e.first_name AS FirstName, e.last_name AS LastName, e.role_id AS Role, e.manager_id AS Manager FROM employee e INNER JOIN employee_role r ON e.role_id = r.id LIMIT 20`;
+	} else if (answer === "add an employee") {
+		inquirer.prompt(questions.addEmployee).then((answer) => {
+			const { firstName, lastName } = answer;
+			chooseRole(firstName, lastName);
+		});
+	} else {
+		selectEmployee();
+	}
+}
+
+async function chooseRole(firstName, lastName) {
+	const select = "SELECT title FROM employee_role";
+	const roleId = await renderedSelect(select);
+	const question = questions.chooseRole;
+	const roleName = await renderedAnswer(roleId, firstName, lastName, question);
+}
+
+async function chooseManager(firstName, lastName, roleName) {
+	inquirer.prompt(questions.addManager).then((answer) => {
+		const { addManager } = answer;
+		if (addManager) {
+			chooseManager(firstName, lastName, roleName);
+		} else {
+			manager = false;
+			addEmployee(firstName, lastName, roleName, manager);
+		}
+	});
+}
+
+async function chooseManager(firstName, lastName, roleName) {
+	const selectName = "SELECT first_name, last_name FROM employee";
+	const employeeList = await renderedSelect(selectName);
+	const question = questions.chooseManager;
+	const getEmployees = async (employeeList) => {
+		let choices = [];
+		const choice = await employeeList.forEach((element) => {
+			choices.push(element.join(" "));
+		});
+		question[0].choices = choices;
+		inquirer
+			.prompt(questions.chooseManager)
+			.then((answer) => {
+				const newEmployee = new Employee();
+				const managerId = newEmployee.selectEmployee(answer);
+				return managerId;
+			})
+			.then((managerId) => {
+				addEmployee(firstName, lastName, roleName, managerId[0].id);
+			});
+	};
+	await getEmployees(employeeList);
+}
+
+async function selectEmployee() {
+	const questionName = questions.Employee;
+	const employeeList = await selectEmployee(questionName);
+	// const selectName = "SELECT first_name, last_name FROM employee";
+	// const employeeList = await renderedSelect(selectName);
+	// const question = questions.Employee;
+	const getEmployees = async (employeeList) => {
+		let choices = [];
+		const choice = await employeeList.forEach((element) => {
+			choices.push(element.join(" "));
+		});
+		question[0].choices = choices;
+		inquirer
+			.prompt(questions.chooseManager)
+			.then((answer) => {
+				const newEmployee = new Employee();
+				const managerId = newEmployee.selectEmployee(answer);
+				return managerId;
+			})
+			.then((managerId) => {
+				updateEmployee(firstName, lastName, roleName, managerId[0].id);
+			});
+	};
+	await getEmployees(employeeList);
+}
+
+async function addEmployee(firstName, lastName, roleName, manager) {
+	if (manager) {
+		const roleId = await getRoleId(roleName);
+		const insert =
+			"INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)";
+		const insertArg = [firstName, lastName, roleId, manager];
+		const newDbConn = new Database();
+		const query = await newDbConn.dbInsert(insert, insertArg);
+	} else {
+		const roleId = await getRoleId(roleName);
+		const insert =
+			"INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)";
+		const insertArg = [firstName, lastName, roleId];
+		const newDbConn = new Database();
+		const query = await newDbConn.dbInsert(insert, insertArg);
+	}
+	initialPrompt();
 }
 
 function initialPrompt() {
-	inquirer.prompt(initialQuestions).then((answer) => {
+	inquirer.prompt(questions.initialQuestions).then((answer) => {
 		const { initialQuestion } = answer;
 		if (
 			initialQuestion === "view all departments" ||
@@ -77,7 +176,7 @@ function initialPrompt() {
 			manageRole(initialQuestion);
 		} else if (
 			initialQuestion === "view all employees" ||
-			initialQuestion === "add a employee" ||
+			initialQuestion === "add an employee" ||
 			initialQuestion === "update an employee role"
 		) {
 			manageEmployee(initialQuestion);
@@ -87,6 +186,46 @@ function initialPrompt() {
 	});
 }
 
+async function renderedSelect(select) {
+	const newDbConn = new Database();
+	const response = await newDbConn.dbRenderedSelect(select);
+	return response;
+}
+
+async function preparedSelect(select, arg) {
+	const consoleLog = false;
+	const newDbConn = new Database();
+	const response = await newDbConn.dbPreparedSelect(select, arg, consoleLog);
+	return response;
+}
+
+async function selectEmployee(questionName) {
+	const selectName = "SELECT first_name, last_name FROM employee";
+	const employeeList = await renderedSelect(selectName);
+	const question = questionName;
+	return employeeList;
+}
+
+async function renderedAnswer(id, name, arg, question) {
+	question[0].choices = id;
+	inquirer.prompt(question).then((answer) => {
+		if (question[0].name === "departmentName") {
+			const { departmentName } = answer;
+			updateRole(name, arg, departmentName);
+		} else if (question[0].name === "roleName") {
+			const { roleName } = answer;
+			chooseManager(name, arg, roleName);
+		}
+	});
+}
+
+async function getRoleId(roleName) {
+	const select = `SELECT id FROM employee_role WHERE title = ?`;
+	const selectArg = roleName.toUpperCase();
+	const roleId = await preparedSelect(select, selectArg);
+	return roleId;
+}
+
 function consoleLogCanceled() {
 	console.log(
 		"Canceled! You can try again by running `node index` in your terminal."
@@ -94,7 +233,7 @@ function consoleLogCanceled() {
 }
 
 function returnToMainMenu() {
-	inquirer.prompt(mainMenu).then((answer) => {
+	inquirer.prompt(questions.mainMenu).then((answer) => {
 		if (answer.mainMenu) {
 			initialPrompt();
 		} else {
